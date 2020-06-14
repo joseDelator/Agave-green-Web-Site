7473fa28490d99bfd0c846c612ead66903e83e0d
jose de la torre <delajose@sonoma.edu>
2020-06-14 08:44:47 -0700

    a upgrades

---
+++ coolstuff/html/navbar.html.js
@@ -0,0 +1,23 @@
+document.write(`
+
+<nav>
+<div class="logo">
+    <h2>Agave Green landscapes</h2>
+</div>
+    
+    <ul class="links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Maitence</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+    </ul>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+        
+    </div>
+</nav>
+
+`);
