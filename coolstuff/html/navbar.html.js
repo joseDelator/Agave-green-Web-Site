46dcb0ea6479172b1436a5a19291797dfc3a3a2f
jose de la torre <delajose@sonoma.edu>
2020-06-22 12:16:09 -0700

    add more images and php

--- coolstuff/html/navbar.html.js
+++ coolstuff/html/navbar.html.js
@@ -1,16 +1,16 @@
 document.write(`
-
+<link rel="stylesheet" href="../css/stylenavbar.css">
 <nav>
 <div class="logo">
     <h2>Agave Green landscapes</h2>
 </div>
     
     <ul class="links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Maitence</a></li>
-        <li><a href="#">Projects</a></li>
+        <li><a href="prime.html">Home</a></li>
+        <li><a href="connectwithus.html">Connect</a></li>
+        <li><a href="gala.html">Projects</a></li>
         <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
+        <li><a href="servies.html">Services</a></li>
     </ul>
     <div class="burger">
         <div class="line1"></div>
@@ -19,5 +19,5 @@ document.write(`
         
     </div>
 </nav>
-
+<script src="../java_stuff/app.js"></script>
 `);
