d678836267073c2d7411ced89c4d843738a6e8e5
joseDelator <49530849+joseDelator@users.noreply.github.com>
2020-07-22 12:34:36 -0700

    indexadded

--- coolstuff/html/navbar.html.js
+++ coolstuff/html/navbar.html.js
@@ -1,15 +1,15 @@
 document.write(`
 <link rel="stylesheet" href="../css/stylenavbar.css">
 <nav>
-<div class="logo">
-    <h2>Agave Green landscapes</h2>
-</div>
+    <div class="logo">
+        <h2>Agave Green landscapes</h2>
+    </div>
     
     <ul class="links">
         <li><a href="prime.html">Home</a></li>
         <li><a href="connectwithus.html">Connect</a></li>
         <li><a href="gala.html">Projects</a></li>
-        <li><a href="#">About</a></li>
+        <li><a href="about.html">About</a></li>
         <li><a href="servies.html">Services</a></li>
     </ul>
     <div class="burger">
